92e470f71be352447aadf1a6fe95a06e8f7f8b65
Roman <roman.smirnov@camunda.com>
2019-02-26 12:32:18 +0100

    feat(camunda/license): provide camunda commercial license header

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var mit = require('./configs/mit');
+var camunda = require('./configs/camunda');
 
 module.exports = {
   configs: {
     mit,
-    MIT: mit
+    MIT: mit,
+    camunda,
+    CAMUNDA: camunda
   }
 };
