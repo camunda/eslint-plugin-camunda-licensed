9c92528685434471ce366403ed9eba28866b808f
Philipp Fromme <philipp.fromme@camunda.com>
2024-10-04 10:02:44 +0200

    feat: migrate to ESLint 9 and flat config
    Related to https://github.com/bpmn-io/internal-docs/issues/1042
    
    BREAKING CHANGE: require eslint@9 (drop support for <= eslint@8)
      BREAKING CHANGE: use from ESM code-base, this module is ESM only
      BREAKING CHANGE: consume as ES module

---
+++ test/apache/eslint.config.js
@@ -0,0 +1,5 @@
+import apache from 'eslint-plugin-camunda-licensed/configs/apache.js';
+
+export default [
+  ...apache
+]
---
+++ test/commercial/eslint.config.js
@@ -0,0 +1,5 @@
+import commercial from 'eslint-plugin-camunda-licensed/configs/commercial.js';
+
+export default [
+  ...commercial
+]
